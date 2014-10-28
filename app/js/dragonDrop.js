09c98c0e39a42161e4928f183604d992e3b9234c
alex-hays-volusion <alex_hays@volusion.com>
2014-10-28 15:19:46 -0500

    remove unused stuff and finish up to a reasonable degree

--- app/js/dragonDrop.js
+++ app/js/dragonDrop.js
@@ -7,7 +7,6 @@
   "use strict";
   var dragAndDropModule = function(draggableElements){
 
-
     var elemYoureDragging = null
         , dataString        = 'text/html' //needs to be changed for IE LOL - also not sure if should change for text/plain etc 
         , startingPosition  = 0    
@@ -20,12 +19,10 @@
           elem.setAttribute('draggable', 'true');
           elem.addEventListener('dragstart', dragonDrop.handleDragStartMove, false);
           elem.addEventListener('dragenter', dragonDrop.handleDragEnter, false);
-          elem.addEventListener('dragover', dragonDrop.handleDragOver, false);
+          elem.addEventListener('dragover',  dragonDrop.handleDragOver, false);
           elem.addEventListener('dragleave', dragonDrop.handleDragLeave, false);
-          elem.addEventListener('drop', dragonDrop.handleDrop, false);
-          elem.addEventListener('dragend', dragonDrop.handleDragEnd, false);
-
-          elem.addEventListener('drop', dragonDrop.handleDrop, false);
+          elem.addEventListener('drop',      dragonDrop.handleDrop, false);
+          elem.addEventListener('dragend',   dragonDrop.handleDragEnd, false);
         };
 
         dragonDrop.handleDragStartMove = function(e) {
@@ -36,67 +33,41 @@
 
           e.dataTransfer.effectAllowed = 'move';
           e.dataTransfer.setData('text/html', this.innerHTML);
-          //e.dataTransfer.setDragImage(dragImg, 20, 50);
+          //e.dataTransfer.setDragImage(dragImg, 20, 50); //idk about a drag image 
         };
 
         dragonDrop.handleDragEnter = function(e) {
-          console.log('drag enter');
-          console.log('E', e);
-
+          if(elementDragged !== this) {
+            this.firstElementChild.style.border = "2px dashed #3a3a3a";
+          }
         };
 
-        dragonDrop.handleDragOver = function(e) {
-          console.log('drag Over');
-          console.log('E', e);     
+        dragonDrop.handleDragOver = function(e) {   
           e.preventDefault();
           e.dataTransfer.dropEffect = 'move';     
         };
 
         dragonDrop.handleDragLeave = function(e) {
-          console.log('drag leave');
-          console.log('E', e);          
+          this.firstElementChild.style.border = "none";
         };
 
         dragonDrop.handleDrop = function(e) {
-          var data = e.dataTransfer.getData('text/html');
-
-          console.log('________');
-          console.log('________');
-          console.log('__drop__');
-          console.log('e', e);
-          console.log('Path', e.path);
-          console.log('Path[0]', e.path[0]);
-          console.log('Path[0].classname', e.path[0].className);
-          console.log('DataTranfer', e.dataTransfer);
-          console.log('Data', data);
-          console.log('srcelement', e.target);
-          console.log('srcelement', e.srcElement);
-          console.log('srcelement', e.srcElement.lastChild);
-          console.log('innerHTML', e.target.innerHTML);
-          console.log('array', draggableElementArray);
-
-          elementDragged.innerHTML = this.innerHTML;
-          this.innerHTML = e.dataTransfer.getData('text/html');
-
-          //e.target
-
+         if(elementDragged !== this) {
+            var data = e.dataTransfer.getData('text/html');
+            elementDragged.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+          }
+          this.firstElementChild.style.border = "none";
           e.stopPropagation();
           return false;
         };
 
         dragonDrop.handleDragEnd = function(e) {
-          console.log('________');
-          console.log('________');
-          console.log('__drag end__');
-          console.log('E', e);
-          console.log('array', draggableElementArray);
-          
+          this.firstElementChild.style.border = "none";
         };
 
         // Actiavte Drag and Dropping on whatever element
         draggableElementArray.forEach(dragonDrop.bindDragAndDropAbilities);
-        
-        
   };
 
   var allDraggableElements = document.querySelectorAll('.draggable-droppable');
